4e63259d61ff4b0fd52e62f339377c923a92b215
Michael Geise <michaelbgeise@gmail.com>
2018-02-08 13:06:53 -0500

    Add lines page

---
+++ lines.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-N2PWQKT');</script>
+	<!-- End Google Tag Manager -->
+	<meta charset="utf-8">
+	<meta name="robots" content="all,follow">
+	<meta name="googlebot" content="index,follow,snippet,archive">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Michael Geise | Lines </title>
+
+	<meta name="description" content="Find out which line is longer">
+	<meta name="author" content="Michael Geise">
+
+	<meta name="keywords" content="">
+
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400,700|Cardo:400,400italic,700">
+
+
+	<!-- Bootstrap and Font Awesome css -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Theme stylesheet -->
+	<link href="css/style.default.css" rel="stylesheet" id="theme-stylesheet">
+
+	<!-- Custom stylesheet - for your changes -->
+	<link href="css/custom.css" rel="stylesheet">
+	<style type="text/css">
+		.answer {
+			font-weight: bold;
+			color: #2a4672;
+			margin-bottom: 30px;
+		}
+	</style>
+	<!-- owl carousel css -->
+
+	<link href="css/owl.carousel.css" rel="stylesheet">
+	<link href="css/owl.theme.css" rel="stylesheet">
+
+	<!-- CSS Animations -->
+	<link href="css/animate.css" rel="stylesheet">
+
+	<!-- Sweet Alerts -->
+	<link rel="stylesheet" type="text/css" href="css/sweetalert2.min.css">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="images/mg-favicon.png">
+
+	<!-- Mordernizr -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+	<!-- Responsivity for older IE -->
+	<!--[if lt IE 9]>
+		<script src="js/respond.min.js"></script>
+	<![endif]-->
+	<!-- Google Analytics -->
+	  <script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-12293005-1', 'auto');
+		ga('send', 'pageview');
+
+	  </script>
+	  <!-- end Google Analytics -->
+
+</head>
+
+<body data-spy="scroll" data-target="#navigation" data-offset="120">
+
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2PWQKT"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
+	<div id="cover">
+		<i class="fa fa-cog fa-spin fa-3x fa-fw"></i>
+		<span class="sr-only">Loading...</span>
+	</div>
+
+	<!-- *** NAVBAR ***
+_________________________________________________________ -->
+	<header>
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="navbar">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand scrollTo" href="../" title="intro section"><img src="images/mg-logo.png" class="img-responsive" alt="MG logo" title="Go to intro section"></a>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- /#navbar -->
+
+	<!-- *** NAVBAR END *** -->
+
+
+	<div id="all">
+			<main>
+			
+			
+
+			<!-- *** question ***
+	_________________________________________________________ -->
+
+			<section class="section contact" id="question" data-animate="bounceIn">
+				<div class="container">
+					<div class="col-md-12">
+
+
+						<h1 class="title">Müller-Lyer Illusion</h1>
+
+						<div class="row">
+							<div class="col-sm-6 col-sm-offset-3 col-xs-12">
+								<h2>Which line is longer?</h2>
+								<img src="images/lines1.png" class="img-responsive" align="Lines Question Illusion" />
+								<p>a)  line 1</p>
+								<p>b)  line 2</p>
+								<p>c)  they’re the same length</p>
+								<p> Find the answer below</p>
+							</div>
+						</div>
+					</div>
+					<!-- /.12 -->
+				</div>
+				<!-- /.container -->
+			</section>
+			<!-- /.section -->
+
+
+
+		<!-- *** question END *** -->
+
+
+				<!-- *** parrallax divider ***
+	_________________________________________________________ -->
+			<div class="section text-parallax parallax-bg">
+			</div>
+			<!-- *** parrallax divider END *** -->
+			<section class="section contact" id="answer" data-animate="bounceIn">
+				<div class="container">
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-sm-6 col-sm-offset-3 col-xs-12">
+								<h2>Answer</h2>
+								<img src="images/lines2.png" class="img-responsive" alt="Lines Answer Illusion" />
+								<p>a)  line 1</p>
+								<p>b)  line 2</p>
+								<p class="answer">Answer: c)  they’re the same length</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-6 col-sm-offset-3 col-xs-12">
+								<p><a href="https://en.wikipedia.org/wiki/M%C3%BCller-Lyer_illusion" target="_blank" title="Wikipedia">Learn more</a> about the Müller-Lyer Illusion</p>
+								<a href="/" title="view micahelgeise.com" class="btn btn-primary btn-lg">More about Michael Geise</a>
+							</div>
+						</div>
+
+					</div>
+					<!-- /.12 -->
+				</div>
+				<!-- /.container -->
+			</section>
+			<!-- /.section -->
+
+		</main>
+
+		<!-- *** answer END *** -->
+
+		<!-- *** FOOTER ***
+_________________________________________________________ -->
+
+		<footer class="section" id="footer">
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-sm-6">
+
+						<p class="social">
+							<a href="https://www.linkedin.com/in/michael-geise-20875623/" target="_blank" class="external linkedin" data-animate-hover="shake" data-animate="fadeInUp"><i class="fa fa-linkedin"></i></a>
+						</p>
+					</div>
+					<!-- /.6 -->
+
+					<div class="col-sm-6">
+						<p>&copy; Michael Geise <?php echo date("Y"); ?>  all rights reserved.</p>
+					</div>
+
+				</div>
+
+			</div>
+			<!-- /.container -->
+		</footer>
+		<!-- /.section -->
+
+		<!-- *** FOOTER END *** -->
+
+
+
+	</div>
+	<!-- /#all -->
+
+	<!-- #### JAVASCRIPT FILES ### -->
+
+	<!-- js base -->
+	<script src="js/jquery-1.11.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- waypoints for scroll spy -->
+	<script src="js/waypoints.min.js"></script>
+
+	<!-- masonry layout -->
+	<script src="js/masonry.pkgd.min.js"></script>
+
+	<!-- owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+
+
+	<!-- jQuery scroll to -->
+	<script src="js/jquery.scrollTo.min.js"></script>
+
+	<!-- jQuery video background -->
+	<script src="js/jquery.vide.js"></script>
+
+
+	<!-- jQuery parallax -->
+	<script src="js/jquery.parallax-1.1.3.js"></script>
+
+	<!-- Sweet Alerts -->
+	<script src="js/sweetalert2.min.js"></script>
+
+	<!-- main js file -->
+
+	<script src="js/front.js"></script>
+
+
+</body>
+
+</html>
