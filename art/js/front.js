ad2f01e1c3ceece85de2f2afaaaa49a867916c7f
Michael Geise <michaelbgeise@gmail.com>
2019-09-10 18:04:27 -0400

    new sitemap, remove random card order, uppercase modals

--- art/js/front.js
+++ art/js/front.js
@@ -33,40 +33,12 @@ $(function () {
         autoplayHoverPause: true
     });
 
-
-    // ------------------------------------------------------ //
-    // For demo purposes, can be deleted
-    // ------------------------------------------------------ //
-
-    var stylesheet = $('link#theme-stylesheet');
-    $( "<link id='new-stylesheet' rel='stylesheet'>" ).insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
-
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
-
-    $("#colour").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, { expires: 365, path: document.URL.substr(0, document.URL.lastIndexOf('/')) });
-
-        }
-
-        return false;
-    });
-
 });
 
 // random card order
- var cards = $(".grid-item");
- for(var i = 0; i < cards.length; i++){
-     var target = Math.floor(Math.random() * cards.length -1) + 1;
-     var target2 = Math.floor(Math.random() * cards.length -1) +1;
-     cards.eq(target).before(cards.eq(target2));
- }
+ // var cards = $(".grid-item");
+ // for(var i = 0; i < cards.length; i++){
+ //     var target = Math.floor(Math.random() * cards.length -1) + 1;
+ //     var target2 = Math.floor(Math.random() * cards.length -1) +1;
+ //     cards.eq(target).before(cards.eq(target2));
+ // }
