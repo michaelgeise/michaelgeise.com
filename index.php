e0cc7d0b91f1a763d4ae453ace65f42eb4fb4791
Michael Geise <michaelbgeise@gmail.com>
2021-03-13 16:22:19 -0500

    mored updates (fix heights and font awesome)

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
         <!-- Favicon-->
         <link rel="icon" type="image/png" href="images/mg-favicon.png">
         <link rel="mask-icon" href="images/mg-logo.svg" color="blue">
-        <link rel="icon" type="image/svg+xml" href="images/mg-logo.svg">
+        <link rel="icon" type="image/svg+xml" href="images/site-poster.png">
         <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
         <!-- Google fonts-->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;1,400&display=swap" rel="stylesheet">
@@ -21,7 +21,7 @@
         <!-- Bootstrap CSS-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <!-- Custom CSS -->
-        <link href="css/style.css?v=2" rel="stylesheet" />
+        <link href="css/style.css?v=2.4" rel="stylesheet" />
         <!-- CSS Animations -->
         <link href="css/animate.min.css" rel="stylesheet">
         <!-- Mordernizr -->
