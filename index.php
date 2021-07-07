581c122e8a5fa94bdd71126ae2aee2fefc5fcb29
Michael Geise <michaelbgeise@gmail.com>
2021-07-07 19:39:21 -0400

    bonus turned to footer

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
         <!-- Bootstrap CSS-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <!-- Custom CSS -->
-        <link href="css/style.css?v=2.92" rel="stylesheet" />
+        <link href="css/style.css?v=2.93" rel="stylesheet" />
         <!-- CSS Animations -->
         <link href="css/animate.min.css" rel="stylesheet">
         <!-- Mordernizr -->
@@ -97,8 +97,8 @@
                 <div class="row">
                     <div class="col-lg-8 mb-3">
                         <p class="masthead-subheading font-weight-light mb-0 mt-3">In college, I majored in business and got a minor in art. From Adobe Cloud and coding to analytics and business processes, I have always had a wide range of interests, which I believe is crucial to understanding the combination of art and science needed in product design.</p>
-                        <p class="masthead-subheading font-weight-light mb-0 mt-3">I love keeping up with the newest product trends and will talk to you about the <a href="https://www.interaction-design.org/literature/topics/design-thinking" target="_blank" title="learn about Design Thinking">Design Thinking</a> process anytime.</p>
-                        <p class="masthead-subheading font-weight-light mb-3 mt-3">I live in the suburbs of Philadelphia, and when I'm not making digital products, I create children's stories, follow local sports, and enjoy nature.</p>
+                        <p class="masthead-subheading font-weight-light mb-0 mt-3">I love keeping up with the newest product trends and will talk to you about the <a href="https://www.interaction-design.org/literature/topics/design-thinking" target="_blank" title="learn about Design Thinking" data-toggle="tooltip" data-placement="top">Design Thinking</a> process anytime.</p>
+                        <p class="masthead-subheading font-weight-light mb-3 mt-3">I live in the suburbs of Philadelphia, and when I'm not making digital products, I create <a href="https://www.goosemonger.com/" target="_blank" title="View Goosemonger Stories" data-toggle="tooltip" data-placement="top"> children's stories</a>, follow local sports, and enjoy nature.</p>
                     </div>
                     <div class="col-lg-4">
                         <div class="card text-center">
@@ -107,7 +107,7 @@
                                     <i class="fab fa-linkedin-in"></i>
                                 </div>
                                 <h3 class="card-title mt-3 mb-3 h4">Contact me or check out my profile on LinkedIn.</h3>
-                                <a class="btn btn-xl btn-block btn-primary mt-3" href="https://www.linkedin.com/in/michael-geise-20875623/">
+                                <a class="btn btn-xl btn-block btn-primary mt-3" title="View Michael's LinkedIn profile" href="https://www.linkedin.com/in/michael-geise-20875623/">
                                     <i class="fas fa-external-link-alt"></i>
                                     View profile
                                 </a>
@@ -123,43 +123,67 @@
             </div>
         </section>
 
-        <!-- Bonus Section-->
-        <section class="page-section" id="bonus" data-animate="fadeIn">
+        <!-- Footer / Copyright Section-->
+        <footer class="copyright py-4">
             <div class="container">
-                <!-- Bonus Section Heading-->
-                <h2 class="page-section-heading text-uppercase mb-3">Bonus</h2>
-                <!-- Bonus Section Content-->
                 <div class="row">
-                    <div class="col-md-12 mb-5 ">
-                        <p class="masthead-subheading font-weight-light mb-0 mt-3">Wow, you're still here?! Thanks for the interest, I'm happy to have you.</p>
-                        <p class="masthead-subheading font-weight-light">Here are some of my side projects as well as some tools and resources I like.</p>
+                    <div class="col-lg-8 mb-3">
+                        <h3 class="text-uppercase mt-5 mb-3 h5">Favorite Resources</h3>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href="https://figma.com/" target="_blank" title="UI & prototyping tool" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Figma</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://miro.com/" target="_blank" title="Digital whiteboard tool" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Miro</a>
+                                    </li>
+                                    <li><a href="https://accessible-colors.com/" target="_blank" title="Accessibility tool" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Accessibe Colors</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href="https://www.nngroup.com/" target="_blank" title="Research-based UX guidance" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Nielsen Norman Group</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.interaction-design.org/" target="_blank" title="Online UX design courses" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Interaction Design Foundation</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.producttalk.org/" target="_blank" title="Product blog and resource" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Product Talk</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 mb-3">
+                         <h3 class="text-uppercase mt-5 mb-3 h5">Passion Projects</h3>
+                         <ul class="list-unstyled">
+                            <li>
+                                <a href="https://www.goosemonger.com/" target="_blank" title="Fantastical fables for lucky children" data-toggle="tooltip" data-placement="top"> <i class="fas fa-external-link-alt"></i> Goosemonger Stories</a>
+                            </li>
+                            <li>
+                                <a href="https://michaelgeise.com/woodlands/" target="_blank" title="Interactive cemetery guide" data-toggle="tooltip" data-placement="top"> <i class="fas fa-external-link-alt"></i> The Woodlands Interactive Map</a>
+                            </li>
+                            <li>
+                                <a href="https://michaelgeise.com/gooseart/" target="_blank" title="Abstract robot-made art" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Gooseart</a>
+                            </li>
+                            <li>
+                                <a href="https://michaelgeise.com/art/" target="_blank" title="Some of my art" data-toggle="tooltip" data-placement="top"> <i class="fas fa-external-link-alt"></i> Fine Art</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <h3 class="text-uppercase mt-5 mb-3">Passion Projects</h3>
-                <?php include 'passion-projects.php';?>
-                
-                    
-                <h3 class="text-uppercase mt-5 mb-3">Favorite Resources</h3>
-                <div class="row fav-links">
-                    <div class="col-md-6">
-                        <p><a href="https://figma.com/" target="_blank" title="UI & prototyping tool" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Figma</a></p>
-                        <p><a href="https://miro.com/" target="_blank" title="Digital whiteboard tool" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Miro</a></p>
-                        <p><a href="https://accessible-colors.com/" target="_blank" title="Accessibility tool" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Accessibe Colors</a></p>
-                    </div>
-                    <div class="col-md-6">
-                        <p><a href="https://www.nngroup.com/" target="_blank" title="Research-based UX guidance" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Nielsen Norman Group</a></p>
-                        <p><a href="https://www.interaction-design.org/" target="_blank" title="Online UX design courses" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Interaction Design Foundation</a></p>
-                        <p><a href="https://www.producttalk.org/" target="_blank" title="Product blog and resource" data-toggle="tooltip" data-placement="top"><i class="fas fa-external-link-alt"></i> Product Talk</a></p>
-                    </div>
-
+                <div class="text-muted mt-5">
+                    <strong>&copy; Michael Geise <?php echo date("Y"); ?>  all rights reserved.</strong>
                 </div>
             </div>
-        </section>
-
-        <!-- Footer / Copyright Section-->
-        <footer class="copyright py-4 text-center">
-            <div class="container"><small>&copy; Michael Geise <?php echo date("Y"); ?>  all rights reserved.</small></div>
         </footer>
+
+    
+
         <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
         <div class="scroll-to-top d-lg-none position-fixed">
             <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
